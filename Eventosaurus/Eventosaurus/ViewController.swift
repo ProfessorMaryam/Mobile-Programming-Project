53a9a41885cd9e83ae354b2cd9b8573031822dcd
ProfessorMaryam <lifeasmaryam7@gmail.com>
2024-12-01 16:46:47 +0300

    Add files via upload

---
+++ Eventosaurus/Eventosaurus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eventosaurus
+//
+//  Created by BP-36-201-14 on 01/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
