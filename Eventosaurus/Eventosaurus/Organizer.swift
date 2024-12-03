c498e34de9a0bd7ff587a58161a1c2fb58d7110a
ProfessorMaryam <lifeasmaryam7@gmail.com>
2024-12-03 18:32:36 +0300

    started UI

--- Eventosaurus/Eventosaurus/Organizer.swift
+++ Eventosaurus/Eventosaurus/Organizer.swift
@@ -1,54 +1,54 @@
+////
+////  Organizer.swift
+////  Eventosaurus
+////
+////  Created by BP-36-201-14 on 01/12/2024.
+////
 //
-//  Organizer.swift
-//  Eventosaurus
+//import Foundation
 //
-//  Created by BP-36-201-14 on 01/12/2024.
+//class Organizer : User{
+//    
+//    
+//    var contactNumber: String
+//    
+//    init(contactNumber: String) {
+//        
+////        super.init(username: <#T##String#>, fullName: <#T##String#>, email: <#T##String#>, dateOfBirth: <#T##String#>, password: <#T##String#>)
+////        self.contactNumber = contactNumber
+//    }
 //
-
-import Foundation
-
-class Organizer : User{
-    
-    
-    var contactNumber: String
-    
-    init(contactNumber: String) {
-        
-        super.init(username: <#T##String#>, fullName: <#T##String#>, email: <#T##String#>, dateOfBirth: <#T##String#>, password: <#T##String#>)
-        self.contactNumber = contactNumber
-    }
-
-    
-    func createEvent(name: String, description: String, date: String, maximumAttendees: Int){
-        let newEvent = Event(status: .upcoming, name: name, description: description, date: date, organizers: self.fullName, maximumAttendees: maximumAttendees)
-
-    }
-    
-    func banEvent(event : Event){
-        event.status = .cancelled
-    }
-    
-    func editEvent(event: Event, newDescription: String?, newDate: String?) {
-           if let newDescription = newDescription {
-               event.description = newDescription
-               print("Event description updated to: \(newDescription)")
-           } 
-        if let newDate = newDate {
-               let dateFormatter = DateFormatter()
-               dateFormatter.dateFormat = "dd-MM-yyyy"
-               if let parsedDate = dateFormatter.date(from: newDate) {
-                   event.date = parsedDate
-                   print("Event date updated to: \(newDate)")
-               } else {
-                   print("Invalid date format. Event date not updated.")
-               }
-           }
-       }
-    
-    
-    
-    func banUserFromEvent(user: User, event: Event) {
-        event.removeUser(user: user)
-        print("\(user.fullName) has been banned from the event '\(event.eventName)'.")
-    }
-}
+//    
+//    func createEvent(name: String, description: String, date: String, maximumAttendees: Int){
+//        let newEvent = Event(status: .upcoming, name: name, description: description, date: date, organizers: self.fullName, maximumAttendees: maximumAttendees)
+//
+//    }
+//    
+//    func banEvent(event : Event){
+//        event.status = .cancelled
+//    }
+//    
+//    func editEvent(event: Event, newDescription: String?, newDate: String?) {
+//           if let newDescription = newDescription {
+//               event.description = newDescription
+//               print("Event description updated to: \(newDescription)")
+//           } 
+//        if let newDate = newDate {
+//               let dateFormatter = DateFormatter()
+//               dateFormatter.dateFormat = "dd-MM-yyyy"
+//               if let parsedDate = dateFormatter.date(from: newDate) {
+//                   event.date = parsedDate
+//                   print("Event date updated to: \(newDate)")
+//               } else {
+//                   print("Invalid date format. Event date not updated.")
+//               }
+//           }
+//       }
+//    
+//    
+//    
+//    func banUserFromEvent(user: User, event: Event) {
+//        event.removeUser(user: user)
+//        print("\(user.fullName) has been banned from the event '\(event.eventName)'.")
+//    }
+//}
--- Eventosaurus/Eventosaurus/ViewController.swift
+++ Eventosaurus/Eventosaurus/ViewController.swift
@@ -11,22 +11,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
+   // @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet var customView: UIView!
     
-
+ 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+     
+    
+        
     }
 
-    let newUser =  User(username: "mrfjm", fullName: "Manaf Mohamed", email: "mrfjm2@gmail.com", dateOfBirth: "17/03/2004", password: "password")
+//    override func viewDidLayoutSubviews(){
+//        super.viewDidLayoutSubviews()
+//        customView.layer.cornerRadius = 25.0
+//        customView.translatesAutoresizingMaskIntoConstraints = true
+//    }
+//    
+    //
+//    let newUser =  User(username: "mrfjm", fullName: "Manaf Mohamed", email: "mrfjm2@gmail.com", dateOfBirth: "17/03/2004", password: "password")
+//    
+//    //newUser.login(username: "mrfjm", password: "password")
+//    
+//    
+//    let event = Event(status:.completed, name: "Food Festival", description: "Food Festival this that this that", date: "31-06-2024", organizers: "organizer 1", maximumAttendees: 200);
+//
+//    
+//    //event.addUser(user: newUser)
     
-    //newUser.login(username: "mrfjm", password: "password")
     
     
-    let event = Event(status:.completed, name: "Food Festival", description: "Food Festival this that this that", date: "31-06-2024", organizers: "organizer 1", maximumAttendees: 200);
-
     
-    //event.addUser(user: newUser)
+    
 
 }
 
