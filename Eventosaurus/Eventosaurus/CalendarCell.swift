b8184cf43c65aa3a1746440160fd1a83dc2be920
BP-36-201-21 <administrator@BP-36-201-21s-iMac.local>
2024-12-04 15:01:58 +0300

    Work in progress

--- Eventosaurus/Eventosaurus/CalendarCell.swift
+++ Eventosaurus/Eventosaurus/CalendarCell.swift
@@ -2,11 +2,35 @@
 //  CalendarCell.swift
 //  Eventosaurus
 //
-//  Created by student on 03/12/2024.
+//  Created by BP-36-201-21 on 04/12/2024.
 //
 
 import UIKit
 
 class CalendarCell: UICollectionViewCell {
     
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var selectedDate = Date()
+    var totalSquares = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setCellsView()
+    }
+    
+    func setCellsView {
+        
+    }
+    
+    @IBAction func previousMonth(_ sender: Any) {
+        
+    }
+    
+    @IBAction func nextMonth(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var dayOfMonth: UILabel!
 }
