c53dafbc03e0475da171502abe9baaa76019c3b7
hx03s <habdulla2903@gmail.com>
2024-12-05 12:05:48 +0300

    Fix issue later

--- Eventosaurus/Eventosaurus/CalendarCell.swift
+++ Eventosaurus/Eventosaurus/CalendarCell.swift
@@ -8,29 +8,5 @@
 import UIKit
 
 class CalendarCell: UICollectionViewCell {
-    
-    @IBOutlet weak var monthLabel: UILabel!
-    @IBOutlet weak var collectionView: UICollectionView!
-    
-    var selectedDate = Date()
-    var totalSquares = [String]()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setCellsView()
-    }
-    
-    func setCellsView {
-        
-    }
-    
-    @IBAction func previousMonth(_ sender: Any) {
-        
-    }
-    
-    @IBAction func nextMonth(_ sender: Any) {
-        
-    }
-    
     @IBOutlet weak var dayOfMonth: UILabel!
 }
