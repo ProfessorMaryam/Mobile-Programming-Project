0583e7587a16318a448fb762fdde2f881dc216e6
thecoolpanda <husain.dsml@gmail.com>
2024-12-10 18:11:48 +0300

    Me

--- Eventosaurus/Eventosaurus/HomePageViewController.swift
+++ Eventosaurus/Eventosaurus/HomePageViewController.swift
@@ -35,16 +35,124 @@ class HomePageViewController: UIViewController {
 
 class FilterSearchViewController: UIViewController {
     
-    @IBOutlet weak var haButton: UIButton!
+    @IBOutlet weak var HAbutton: UIButton!
+    
+    @IBOutlet weak var SPButton: UIButton!
+    
+    @IBOutlet weak var FNButton: UIButton!
+    
+    @IBOutlet weak var ENButton: UIButton!
+    
+    @IBOutlet weak var ARButton: UIButton!
+    
+    @IBOutlet weak var CMButton: UIButton!
+    
+    @IBOutlet weak var MSButton: UIButton!
+    
+    @IBOutlet weak var LTButton: UIButton!
+    
+    @IBOutlet weak var PHButton: UIButton!
+    
+    @IBOutlet weak var EDButton: UIButton!
+    
+    @IBOutlet weak var SLButton: UIButton!
+    
+    @IBOutlet weak var FDButton: UIButton!
+    
+    
+    
+    @IBOutlet weak var NRButton: UIButton!
+    
+    @IBOutlet weak var CLButton: UIButton!
+    
+    @IBOutlet weak var MQButton: UIButton!
+    
+    @IBOutlet weak var SRButton: UIButton!
+    
+    
+    
+    
+    @IBOutlet weak var WEButton: UIButton!
+    
+    @IBOutlet weak var MOButton: UIButton!
+    
+    
+    
+    
+    var HAChecked = false
+
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-       
     }
     
-    @IBAction func HAButton(_ sender: UIButton) {
+    @IBAction func HATapped(_ sender: UIButton) {
+        HAChecked = !HAChecked
+        let imageName = HAChecked ? "checkmark.square.fill" : "square"
+        let image = UIImage(systemName: imageName)
+        HAbutton.setImage(image, for: .normal)
     }
+    
+    @IBAction func PHTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func SPTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func FNTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func ENTapped(_ sender: Any) {
+    }
+    
+    @IBAction func ARTapped(_ sender: Any) {
+    }
+    
+    @IBAction func CMTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func EDTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func SLTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func FDTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func LRTapped(_ sender: Any) {
+    }
+    
+    @IBAction func MSTapped(_ sender: UIButton) {
+    }
+    
+    
+    
+    @IBAction func NRTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func CLTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func MQTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func SRTapped(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    @IBAction func WETapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func MOTapped(_ sender: UIButton) {
+    }
+    
+    
     
 }
 
