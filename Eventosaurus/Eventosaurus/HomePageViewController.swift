5830032c673796d6794b9107bd9f7d78eb80e5ba
thecoolpanda <husain.dsml@gmail.com>
2024-12-11 09:43:07 +0300

    Back in the 90s, i was in a very famous tv show.

--- Eventosaurus/Eventosaurus/HomePageViewController.swift
+++ Eventosaurus/Eventosaurus/HomePageViewController.swift
@@ -36,50 +36,68 @@ class HomePageViewController: UIViewController {
 class FilterSearchViewController: UIViewController {
     
     @IBOutlet weak var HAbutton: UIButton!
+    var HAChecked = false
     
     @IBOutlet weak var SPButton: UIButton!
+    var SPChecked = false
     
     @IBOutlet weak var FNButton: UIButton!
+    var FNChecked = false
     
     @IBOutlet weak var ENButton: UIButton!
+    var ENChecked = false
     
     @IBOutlet weak var ARButton: UIButton!
+    var ARChecked = false
     
     @IBOutlet weak var CMButton: UIButton!
+    var CMChecked = false
     
     @IBOutlet weak var MSButton: UIButton!
+    var MSChecked = false
     
     @IBOutlet weak var LTButton: UIButton!
+    var LTChecked = false
     
     @IBOutlet weak var PHButton: UIButton!
+    var PHChecked = false
     
     @IBOutlet weak var EDButton: UIButton!
+    var EDChecked = false
     
     @IBOutlet weak var SLButton: UIButton!
+    var SLChecked = false
     
     @IBOutlet weak var FDButton: UIButton!
+    var FDChecked = false
     
     
     
     @IBOutlet weak var NRButton: UIButton!
+    var NRChecked = false
     
     @IBOutlet weak var CLButton: UIButton!
+    var CLChecked = false
     
     @IBOutlet weak var MQButton: UIButton!
+    var MQChecked = false
     
     @IBOutlet weak var SRButton: UIButton!
+    var SRChecked = false
     
     
     
     
     @IBOutlet weak var WEButton: UIButton!
+    var WEChecked = false
     
     @IBOutlet weak var MOButton: UIButton!
+    var MOChecked = false
     
     
     
     
-    var HAChecked = false
+    
 
     
     
@@ -96,60 +114,128 @@ class FilterSearchViewController: UIViewController {
     }
     
     @IBAction func PHTapped(_ sender: UIButton) {
+        PHChecked = !PHChecked
+        let imageName = PHChecked ? "checkmark.square.fill" : "square"
+        let image = UIImage(systemName: imageName)
+        PHButton.setImage(image, for: .normal)
     }
     
     @IBAction func SPTapped(_ sender: UIButton) {
+        SPChecked = !SPChecked
+        let imageName = SPChecked ? "checkmark.square.fill" : "square"
+        let image = UIImage(systemName: imageName)
+        SPButton.setImage(image, for: .normal)
     }
     
     @IBAction func FNTapped(_ sender: UIButton) {
+        FNChecked = !FNChecked
+        let imageName = FNChecked ? "checkmark.square.fill" : "square"
+        let image = UIImage(systemName: imageName)
+        FNButton.setImage(image, for: .normal)
     }
     
     @IBAction func ENTapped(_ sender: Any) {
+        ENChecked = !ENChecked
+        let imageName = ENChecked ? "checkmark.square.fill" : "square"
+        let image = UIImage(systemName: imageName)
+        ENButton.setImage(image, for: .normal)
     }
     
     @IBAction func ARTapped(_ sender: Any) {
+        ARChecked = !ARChecked
+        let imageName = ARChecked ? "checkmark.square.fill" : "square"
+        let image = UIImage(systemName: imageName)
+        ARButton.setImage(image, for: .normal)
     }
     
     @IBAction func CMTapped(_ sender: UIButton) {
+        CMChecked = !CMChecked
+        let imageName = CMChecked ? "checkmark.square.fill" : "square"
+        let image = UIImage(systemName: imageName)
+        CMButton.setImage(image, for: .normal)
     }
     
     @IBAction func EDTapped(_ sender: UIButton) {
+        EDChecked = !EDChecked
+        let imageName = EDChecked ? "checkmark.square.fill" : "square"
+        let image = UIImage(systemName: imageName)
+        EDButton.setImage(image, for: .normal)
     }
     
     @IBAction func SLTapped(_ sender: UIButton) {
+        SLChecked = !SLChecked
+        let imageName = SLChecked ? "checkmark.square.fill" : "square"
+        let image = UIImage(systemName: imageName)
+        SLButton.setImage(image, for: .normal)
     }
     
     @IBAction func FDTapped(_ sender: UIButton) {
+        FDChecked = !FDChecked
+        let imageName = FDChecked ? "checkmark.square.fill" : "square"
+        let image = UIImage(systemName: imageName)
+        FDButton.setImage(image, for: .normal)
     }
     
     @IBAction func LRTapped(_ sender: Any) {
+        LTChecked = !LTChecked
+        let imageName = LTChecked ? "checkmark.square.fill" : "square"
+        let image = UIImage(systemName: imageName)
+        LTButton.setImage(image, for: .normal)
     }
     
     @IBAction func MSTapped(_ sender: UIButton) {
+        MSChecked = !MSChecked
+        let imageName = MSChecked ? "checkmark.square.fill" : "square"
+        let image = UIImage(systemName: imageName)
+        MSButton.setImage(image, for: .normal)
     }
     
     
     
     @IBAction func NRTapped(_ sender: UIButton) {
+        NRChecked = !NRChecked
+        let imageName = NRChecked ? "checkmark.square.fill" : "square"
+        let image = UIImage(systemName: imageName)
+        NRButton.setImage(image, for: .normal)
     }
     
     
     @IBAction func CLTapped(_ sender: UIButton) {
+        CLChecked = !CLChecked
+        let imageName = CLChecked ? "checkmark.square.fill" : "square"
+        let image = UIImage(systemName: imageName)
+        CLButton.setImage(image, for: .normal)
     }
     
     @IBAction func MQTapped(_ sender: UIButton) {
+        MQChecked = !MQChecked
+        let imageName = MQChecked ? "checkmark.square.fill" : "square"
+        let image = UIImage(systemName: imageName)
+        MQButton.setImage(image, for: .normal)
     }
     
     @IBAction func SRTapped(_ sender: UIButton) {
+        SRChecked = !SRChecked
+        let imageName = SRChecked ? "checkmark.square.fill" : "square"
+        let image = UIImage(systemName: imageName)
+        SRButton.setImage(image, for: .normal)
     }
     
     
     
     
     @IBAction func WETapped(_ sender: UIButton) {
+        WEChecked = !WEChecked
+        let imageName = WEChecked ? "checkmark.square.fill" : "square"
+        let image = UIImage(systemName: imageName)
+        WEButton.setImage(image, for: .normal)
     }
     
     @IBAction func MOTapped(_ sender: UIButton) {
+        MOChecked = !MOChecked
+        let imageName = MOChecked ? "checkmark.square.fill" : "square"
+        let image = UIImage(systemName: imageName)
+        MOButton.setImage(image, for: .normal)
     }
     
     
