a2fa202d728a1e7c2dc1b9a434ce15356ec20d1a
Hasan <habdulla2903@gmail.com>
2024-12-14 16:59:22 +0300

    Calendar works

---
+++ Eventosaurus/Eventosaurus/NotificationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NotificationViewController.swift
+//  Eventosaurus
+//
+//  Created by Hx on 14/12/2024.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    
+
+}
--- Eventosaurus/Eventosaurus/ViewController.swift
+++ Eventosaurus/Eventosaurus/ViewController.swift
@@ -1,19 +1,9 @@
-//
-//  ViewController.swift
-//  Eventosaurus
-//
-//  Created by BP-36-201-14 on 01/12/2024.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController{
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
-
