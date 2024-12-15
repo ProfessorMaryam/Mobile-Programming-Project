ac5227cd79db35ac903deb42c8aee10a6a7da98c
Hasan <habdulla2903@gmail.com>
2024-12-15 16:51:33 +0300

    Calendar is done, fixing notification

--- Eventosaurus/Eventosaurus/NotificationViewController.swift
+++ Eventosaurus/Eventosaurus/NotificationViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class NotificationViewController: UIViewController {
+class NotificationViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var nameArray: [String] = []
+    var imageArray: [UIImage] = []
+    var descriptionArray: [String] = []
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    override func viewDidLoad() {super.viewDidLoad()
+        
+        nameArray = ["Fun Land"]
+        imageArray = [UIImage(systemName: "gear")!]
+        descriptionArray = ["This is a test"]
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameArray.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "NotificationCell") as! notificationTableViewCell
+        
+        cell.eventImage.image = imageArray[indexPath.row]
+        cell.eventTitle.text = nameArray[indexPath.row]
+        
+        return cell
+    }
 
 }
---
+++ Eventosaurus/Eventosaurus/notificationTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  notificationTableViewCell.swift
+//  Eventosaurus
+//
+//  Created by Hx on 15/12/2024.
+//
+
+import UIKit
+
+class notificationTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var eventImage: UIImageView!
+    
+    @IBOutlet weak var eventTitle: UILabel!
+    
+    @IBOutlet weak var eventDescription: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        
+    }
+
+}
