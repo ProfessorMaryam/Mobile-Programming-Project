a958ccaee76de3901d5768c457a39dac8e5e5279
Nimbbus <mohammad-karim@hotmail.com>
2024-12-24 14:28:17 +0300

    Did the coding part for create event page and connected it to the firebase collection and the event to document and the fields to the text fields but i still didnt check if it works

--- Eventosaurus/Eventosaurus/CreateEventViewController.swift
+++ Eventosaurus/Eventosaurus/CreateEventViewController.swift
@@ -6,9 +6,24 @@
 //
 
 import UIKit
-
+import FirebaseFirestore
 class CreateEventViewController: UIViewController {
 
+    
+    @IBOutlet weak var eventNameTextField: UITextField!
+    
+    @IBOutlet weak var eventDateTextField: UITextField!
+    
+    @IBOutlet weak var organizerTextField: UITextField!
+    
+    @IBOutlet weak var coOrganizerTextField: UITextField!
+    
+    @IBOutlet weak var maxAttendeesTextField: UITextField!
+    
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    let db = Firestore.firestore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,6 +31,53 @@ class CreateEventViewController: UIViewController {
     }
     
 
+    @IBAction func submitButtonTapped(_ sender: UIButton) {
+        createEvent()
+    }
+    
+    
+    
+    func createEvent() {
+            guard let eventName = eventNameTextField.text,
+                  let eventDate = eventDateTextField.text,
+                  let organizer1 = organizerTextField.text,
+                  let organizer2 = coOrganizerTextField.text,
+                  let maxAttendeesString = maxAttendeesTextField.text,
+                  let maxAttendees = Int(maxAttendeesString),
+                  let description = descriptionTextView.text, !description.isEmpty else {
+                // Handle the case where text fields are empty or invalid
+                print("Please fill in all fields correctly.")
+                return
+            }
+
+            let eventData: [String: Any] = [
+                "Event Name": eventName,
+                "Date": eventDate,
+                "Organizer1": organizer1,
+                "Organizer2": organizer2,
+                "Maximum Attendees": maxAttendees,
+                "Description": description
+            ]
+
+            db.collection("Events").addDocument(data: eventData) { error in
+                if let error = error {
+                    print("Error adding Event: \(error)")
+                } else {
+                    print("Event added successfully!")
+                    // Optionally, clear the text fields or navigate to another screen
+                    self.clearFields()
+                }
+            }
+        }
+
+        func clearFields() {
+            eventNameTextField.text = ""
+            eventDateTextField.text = ""
+            organizerTextField.text = ""
+            coOrganizerTextField.text = ""
+            maxAttendeesTextField.text = ""
+            descriptionTextView.text = "" // Clear the UITextView
+        }
     /*
     // MARK: - Navigation
 
