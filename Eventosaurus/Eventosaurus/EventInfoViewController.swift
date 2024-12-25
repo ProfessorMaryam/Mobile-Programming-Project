064db3901e8a57f815319d01a837a79ac091f5c2
ProfessorMaryam <lifeasmaryam7@gmail.com>
2024-12-25 17:27:40 +0300

    Removed unecessary storyboard references

---
+++ Eventosaurus/Eventosaurus/EventInfoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EventInfoViewController.swift
+//  Eventosaurus
+//
+//  Created by BP-36-201-14 on 25/12/2024.
+//
+
+import UIKit
+
+class EventInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
