8b3135f60dbb239524aab315160ab09402baa577
Nimbbus <mohammad-karim@hotmail.com>
2024-12-26 16:19:06 +0300

    fix conflict

--- Eventosaurus/Eventosaurus/AppDelegate.swift
+++ Eventosaurus/Eventosaurus/AppDelegate.swift
@@ -13,16 +13,17 @@ import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Configure Firebase
         FirebaseApp.configure()
-
+        
         // Override point for customization after application launch.
         return true
     }
+    
     // MARK: UISceneSession Lifecycle
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, optons: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
@@ -34,6 +35,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
