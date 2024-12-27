780d56afee1984d117da3573964e912ea03085a9
thecoolpanda <husain.dsml@gmail.com>
2024-12-27 18:03:55 +0300

    try

--- Eventosaurus/Eventosaurus/ViewController.swift
+++ Eventosaurus/Eventosaurus/ViewController.swift
@@ -7,52 +7,17 @@
 
 import UIKit
 
-
-
 class ViewController: UIViewController {
     
    // @IBOutlet weak var loginButton: UIButton!
     @IBOutlet var customView: UIView!
     
- 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func unwindToStartup(unwindSegue: UIStoryboardSegue){
         
     }
-
-//    override func viewDidLayoutSubviews(){
-//        super.viewDidLayoutSubviews()
-//        customView.layer.cornerRadius = 25.0
-//        customView.translatesAutoresizingMaskIntoConstraints = true
-//    }
-//    
-    //
-//    let newUser =  User(username: "mrfjm", fullName: "Manaf Mohamed", email: "mrfjm2@gmail.com", dateOfBirth: "17/03/2004", password: "password")
-//    
-//    //newUser.login(username: "mrfjm", password: "password")
-//    
-//    
-//    let event = Event(status:.completed, name: "Food Festival", description: "Food Festival this that this that", date: "31-06-2024", organizers: "organizer 1", maximumAttendees: 200);
-//
-//    
-//    //event.addUser(user: newUser)
-    
-    
-    
-    
-    
-
-}
-
-
-
-
-class RegisterPageViewController: UIViewController{
-    
-    @IBOutlet weak var collectionView: UICollectionView!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-        
+
 }
+
