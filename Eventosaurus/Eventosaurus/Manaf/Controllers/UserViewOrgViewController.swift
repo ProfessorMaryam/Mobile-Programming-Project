d30ad8e22c6f5c0279b8f583c01e4e1dbbaf5969
pinkberrybrain <pinkberrybrain@gmail.com>
2024-12-27 18:31:04 +0300

    the followers new push.

--- Eventosaurus/Eventosaurus/Manaf/Controllers/UserViewOrgViewController.swift
+++ Eventosaurus/Eventosaurus/Manaf/Controllers/UserViewOrgViewController.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Manaf Mohamed on 25/12/2024.
 //
-
 import UIKit
 
 class UserViewOrgViewController: UIViewController {
@@ -13,51 +12,51 @@ class UserViewOrgViewController: UIViewController {
     @IBOutlet weak var labelFollowers: UILabel!
     
     var followerCount: Int {
-            get {
-                // Retrieve follower count from UserDefaults or default to 100 if not found
-                return UserDefaults.standard.integer(forKey: "followerCount")
-            }
-            set {
-                // Save the new follower count to UserDefaults
-                UserDefaults.standard.set(newValue, forKey: "followerCount")
-            }
-        }
-    
-    
-    var isFollowing = false
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        labelFollowers.text = "\(followerCount)"
-        
-        followButton.setTitle(isFollowing ? "Following" : "Follow", for: .normal)
+               get {
+                   // Retrieve follower count from UserDefaults or default to 100 if not found
+                   return UserDefaults.standard.integer(forKey: "followerCount")
+               }
+               set {
+                   // Save the new follower count to UserDefaults
+                   UserDefaults.standard.set(newValue, forKey: "followerCount")
+               }
+           }
+       
+       
+       var isFollowing = false
+       
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
            
-        
-    }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let organizerProfileVC = segue.destination as? OrganizerProfileViewController {
-            organizerProfileVC.followerCount = followerCount
-        }
-        
-    }
-    
-    @IBAction func followButtonTapped(_ sender: UIButton) {
-        
-        if isFollowing {
+           labelFollowers.text = "\(followerCount)"
+           
+           followButton.setTitle(isFollowing ? "Following" : "Follow", for: .normal)
+              
+           
+       }
+       override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+           if let organizerProfileVC = segue.destination as? OrganizerProfileViewController {
+               organizerProfileVC.followerCount = followerCount
+           }
+           
+       }
+       
+       @IBAction func followButtonTapped(_ sender: UIButton) {
+           // Toggle the following state
+           if isFollowing {
 
-            followButton.setTitle("Follow", for: .normal)
-            followerCount -= 1
-            isFollowing = false
-            
-        } else {
-         
-            followButton.setTitle("Following", for: .normal)
-            followerCount += 1
-            isFollowing = true
-            
-        }
-        labelFollowers.text = "\(followerCount)"
-    }
-}
+                      followButton.setTitle("Follow", for: .normal)
+                      followerCount -= 1
+                      isFollowing = false
+                      
+                  } else {
+                   
+                      followButton.setTitle("Following", for: .normal)
+                      followerCount += 1
+                      isFollowing = true
+                      
+                  }
+                  labelFollowers.text = "\(followerCount)"
+              }
+          }  
